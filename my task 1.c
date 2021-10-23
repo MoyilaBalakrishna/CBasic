726989e8e67d64b675a6110e13bff5d1547293c5
MoyilaBalakrishna <92074921+MoyilaBalakrishna@users.noreply.github.com>
2021-10-23 10:59:28 +0530

    Add files via upload

---
+++ my task 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+	//variable decleration
+	float number1, number2, sum;
+	//intilization
+	number1=15.5;
+	number2=50.7;
+	//calculation
+	sum= number1+number2; // 15.5+50.7 =66.2
+	//out put
+	printf("sum of %f and %f is %f",number1,number2,sum);
+}
---
+++ student details.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main ()
+{
+	//variable declaration
+	int age; 
+	float cgpa;
+	char intial;
+	//intialation
+	age=20;
+	cgpa=8.5;
+	intial='M';
+	//OUTPUT
+	printf("Age of the student is %i\n",age);
+	printf("cgpa of the student is%f\n", cgpa);
+	printf("surname of the student %c", intial);
+}
