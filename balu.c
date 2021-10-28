90d3852ae943ac91932c0894a965a6870fc03374
MoyilaBalakrishna <92074921+MoyilaBalakrishna@users.noreply.github.com>
2021-10-28 10:32:36 +0530

    Add files via upload

---
+++ balu.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("name: M.Balakrishna\n Roll.no: 20MH5A0331\n college: Aditya college of engineering\n Branch:Mechanical");
+}
---
+++ balu@ntr-1.c
@@ -0,0 +1,23 @@
+//student mwrks
+#include<stdio.h>
+void main()
+{
+  //varible decleration
+  int phy,che,mat,eng,tot;
+  //intilization
+  phy=78;
+  che=98;
+  mat=95;
+  eng=78;
+  //calcalation
+  tot=phy+che+mat+eng;
+  //output
+  printf("marks of student: M.Balakrishna\n");
+  printf("marks in physics %i \n",phy);
+  printf("marks in chemistary %i \n",che);
+  printf("marks in mathematics %i \n",mat);
+  printf("marks in english %i \n",eng);
+  printf("total marks of student%i\n",tot);
+}
+  
+  
