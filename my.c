f264be01a5ca0e0264822ea814709ad22c84be3e
MoyilaBalakrishna <92074921+MoyilaBalakrishna@users.noreply.github.com>
2021-10-29 10:01:47 +0530

    Add files via upload

---
+++ my.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("Myname is M.BALAKRISHNA\n Studing in ADITYA COLLEGE OF ENGINEERING \n roll no 20MH5A0331\n studing in branch  MECHANICAL \n 	I am from  srikakulam disrict");
+}
