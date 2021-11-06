38fb3f0bf82eabc7357201bfe7f8da30dfe662f0
MoyilaBalakrishna <92074921+MoyilaBalakrishna@users.noreply.github.com>
2021-11-06 10:40:10 +0530

    Add files via upload

---
+++ arthematic natural no 3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	// variable decleration 
+	int n,k;
+	n= 10;
+	//formula 
+    k= n*(n+1)/2;
+	 // out put
+	 printf("sum of %d natural number is %d",n,k); 
+}
---
+++ arthmaticd symbols.c
@@ -0,0 +1,19 @@
+// printing all arthmatic operaions
+#include<stdio.h>
+void main()
+{
+	printf("THE NUMERICAL VALUES FOR PRINTING ALL ARTHMETIC OPERATIONS \n A= 11\n B= 2\n");
+	int a,b,c,d,f,g,h;
+	a= 11;
+	b=2;
+	c= a+b;
+	d= a-b;
+	f= a*b;
+	g= a/b;
+	h= a%b;
+	printf("sum of %d and %d is %d\n",a,b,c);
+	printf("difference of %d and %d is%d\n",a,b,d);
+	printf("product of %d and %d is %d\n",a,b,f);
+	printf("division of %d and %d is %d\n",a,b,g);
+	printf("reminder of %d and %d is %d",a,b,h);
+}
